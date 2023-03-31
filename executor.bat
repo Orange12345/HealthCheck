2d099a575b5f27bf1ed603bb2f46ebbd2b2244ed
Babloo Kumar <kumarbabloo1031@gmail.com>
2023-03-31 16:05:23 +0530

    Add files via upload

---
+++ executor.bat
@@ -0,0 +1,4 @@
+:loop
+D:\Babloo\POC\HealthCheck\RM_Utility.html
+timeout /t 30 /nobreak
+goto :loop
---
+++ url.js
@@ -0,0 +1,35 @@
+var urls =[
+            // DIT Server 2
+            {
+                id: "dit_app-provider",
+                url: "http://localhost:8081/q/health"
+            },
+			{
+                id: "dit_app-member",
+                url: "http://localhost:8080/q/health"
+            },
+			{
+                id: "dit_app-member",
+                url: "http://localhost:8080/q/health"
+            },
+			{
+                id: "dit_app-Dummy",
+                url: "http://10.111.12.212:9202/app-provider/health"
+            },
+			{
+                id: "st_app-provider",
+                url: "http://10.111.12.212:9202/app-provider/health"
+            },
+			{
+                id: "uat_app-provider",
+                url: "http://10.111.12.20:9202/app-provider/health"
+            },
+			{
+                id: "nfr1_app-provider",
+                url: "http://10.111.12.22:9128/app-provider/health"
+            },
+			{
+                id: "prod1_app-provider",
+                url: "http://10.111.12.22:9128/app-provider/health"
+            }
+        ];
